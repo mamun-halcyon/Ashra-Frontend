c2150d812c572017c5d84a80d72faf2a866a4d0d
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-08-16 19:20:55 +0600

    change heading

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,11 +4,17 @@ import { BiSolidPhone } from 'react-icons/Bi';
 const TopHeader = () => {
   return (
     <div className=" flex justify-between items-center py-4">
-      <div className="flex justify-between items-center">
+      <div className="flex  items-center">
         <span className=" mr-2">
           <BiSolidPhone />
         </span>
         <p>+880 1766 688840</p>
+        <div className="flex  items-center ml-4">
+          <span className=" mr-2">
+            <BiSolidPhone />
+          </span>
+          <p>10:00 AM - 6:00 PM | Sat - Thus</p>
+        </div>
       </div>
       <div>2</div>
     </div>
