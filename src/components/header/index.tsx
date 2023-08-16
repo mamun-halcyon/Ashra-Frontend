f834e1c8435f1d65a68d145601bad012e1e67d04
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-08-16 19:21:50 +0600

    top header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,34 @@
+import Link from 'next/link';
 import React from 'react';
 import { BiSolidPhone } from 'react-icons/Bi';
+import { AiFillBell } from 'react-icons/ai';
 
 const TopHeader = () => {
   return (
     <div className=" flex justify-between items-center py-4">
       <div className="flex  items-center">
-        <span className=" mr-2">
+        <span className=" mr-2 text-primary">
           <BiSolidPhone />
         </span>
-        <p>+880 1766 688840</p>
+        <p className="text-primary">+880 1766 688840</p>
         <div className="flex  items-center ml-4">
-          <span className=" mr-2">
-            <BiSolidPhone />
+          <span className=" mr-2 text-primary">
+            <AiFillBell />
           </span>
-          <p>10:00 AM - 6:00 PM | Sat - Thus</p>
+          <p className=" text-primary">10:00 AM - 6:00 PM | Sat - Thus</p>
         </div>
       </div>
-      <div>2</div>
+      <div>
+        <Link className="mr-2 text-primary" href={'/abut'}>
+          Help
+        </Link>
+        <Link className="mr-2 text-primary" href={'/'}>
+          Login
+        </Link>
+        <Link className="mr-2 text-primary" href={'/'}>
+          Registration
+        </Link>
+      </div>
     </div>
   );
 };
