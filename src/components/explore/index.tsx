90fc0f856d7f43f1f5a9bac09437cfb1afc8967e
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-08-28 14:18:50 +0600

    explore pard added

--- src/components/explore/index.tsx
+++ src/components/explore/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import './index.scss';
 
 interface IExplore {
   image: string;
@@ -18,15 +19,15 @@ const ExploreCard: React.FC<IProps> = ({
 }): React.JSX.Element => {
   return (
     <div className={`${className} explore-card`}>
-      <Link href={href ? href : '/'}>
+      <Link className="explore-item" href={href ? href : '/'}>
         <Image
           src={`/assets/images/explore/${item.image}`}
-          width={300}
-          height={200}
+          width={150}
+          height={150}
           alt="explore"
         />
+        <p className=" mt-5 text-base font-gotham font-normal">{item.title}</p>
       </Link>
-      <p className=" mt-5 text-lg font-gotham font-normal">{item.title}</p>
     </div>
   );
 };
