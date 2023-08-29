84c6f2c3f5570354a545b1502f80d26a7060e95c
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-08-29 13:09:23 +0600

    transition

--- src/components/megamenu/index.tsx
+++ src/components/megamenu/index.tsx
@@ -16,7 +16,7 @@ const MegaMenu = () => {
               <div key={i}>
                 <div className="mr-2 text-left md:cursor-pointer group">
                   <div
-                    className="py-1 px-5 font-gotham font-medium text-base flex justify-between items-center  pr-5 group border border-black text-black hover:border-primary hover:text-primary transition-all duration-75"
+                    className="py-1 px-5 font-gotham font-medium text-base flex justify-between items-center  pr-5 group border border-black text-black hover:border-primary hover:text-primary transition-all"
                     onClick={() => {
                       heading !== link.name
                         ? setHeading(link.name)
