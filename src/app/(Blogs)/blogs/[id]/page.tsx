1c76f563df068fdafb1f70d1432fdc861b104c7f
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-05 15:34:51 +0600

    blogs pages

--- src/app/(Blogs)/blogs/[id]/page.tsx
+++ src/app/(Blogs)/blogs/[id]/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const BlogDetails = () => {
-  return <section>BlogDetails</section>;
+  return (
+    <section>
+      <div className="container">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+        consequatur eligendi neque nam commodi nesciunt quia debitis, sunt iure
+        harum quas ratione ut dolores. Veniam quaerat culpa voluptatem hic
+        suscipit!
+      </div>
+    </section>
+  );
 };
 
 export default BlogDetails;
