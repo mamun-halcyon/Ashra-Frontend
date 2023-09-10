545b7448a92fff72c293ec1309c28e0357c7680f
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-10 13:11:07 +0600

    social item

--- src/app/(Blogs)/blogs/[id]/page.tsx
+++ src/app/(Blogs)/blogs/[id]/page.tsx
@@ -2,6 +2,11 @@ import { BlogData } from '@/static/BlogData';
 import Image from 'next/image';
 import './page.scss';
 import BlogSideCard from '@/components/blog-side-card';
+import { BiLogoFacebook } from 'react-icons/bi';
+import { FiInstagram } from 'react-icons/fi';
+import Link from 'next/link';
+import { BsLinkedin } from 'react-icons/bs';
+import { AiOutlineTwitter } from 'react-icons/ai';
 
 const BlogDetails = ({ params }: { params: { id: string } }) => {
   const data = BlogData.filter((blog) => blog.id === params.id);
@@ -19,18 +24,43 @@ const BlogDetails = ({ params }: { params: { id: string } }) => {
                 height={600}
                 alt="blog"
               />
-              <div className="border-area p-4">
-                <h2 className="font-gotham font-bold text-base text-black">
-                  {blogData.title}
-                </h2>
-                <div className="flex justify-between">
-                  <div>Icon</div>
-                  <div>28 Aug 2023</div>
+              <div className=" p-4">
+                <div className="border-area">
+                  <h2 className="font-gotham font-bold text-base text-black">
+                    {blogData.title}
+                  </h2>
+                  <div className="flex justify-between py-2 social">
+                    <div className="flex">
+                      <div className="social-item flex justify-center items-center mr-1">
+                        <Link href={'/'}>
+                          <BiLogoFacebook />
+                        </Link>
+                      </div>
+                      <div className="social-item flex justify-center items-center mr-1">
+                        <Link href={'/'}>
+                          <FiInstagram />
+                        </Link>
+                      </div>
+                      <div className="social-item flex justify-center items-center mr-1">
+                        <Link href={'/'}>
+                          <BsLinkedin />
+                        </Link>
+                      </div>
+                      <div className="social-item flex justify-center items-center mr-1">
+                        <Link href={'/'}>
+                          <AiOutlineTwitter />
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="font-gotham font-normal text-xs text-black">
+                      28 Aug 2023
+                    </div>
+                  </div>
                 </div>
               </div>
               <div>
                 <div
-                  className="font-gotham font-normal text-xs mt-3"
+                  className="font-gotham font-normal text-xs mt-3 px-4"
                   dangerouslySetInnerHTML={{
                     __html: blogData.description ?? '',
                   }}
