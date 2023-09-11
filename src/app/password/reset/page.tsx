b3b4efedb09037b149d5f2550444e58729d68ab4
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-11 16:47:00 +0600

    gfhgf

---
+++ src/app/password/reset/page.tsx
@@ -0,0 +1,35 @@
+import Button from '@/components/button';
+import FormGroup from '@/components/fromgroup';
+import './page.scss';
+
+function ResetPassword() {
+  return (
+    <main>
+      <section className="flex justify-center items-center forget-page">
+        <div className="w-[400px] forget-area px-4 py-6 ">
+          <h2 className=" font-gotham font-normal text-xl text-black">
+            Forgot password?
+          </h2>
+          <p className="font-gotham font-light text-sm text-black mt-2">
+            Enter your email address to recover your password.
+          </p>
+          <form>
+            <FormGroup
+              className="mt-2 "
+              type="email"
+              title="E-Mail*"
+              placeholder="Your Email"
+              required
+            />
+
+            <Button className="w-full py-1 mt-3 font-gotham font-normal text-base forget-button">
+              Send Password Reset Link
+            </Button>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default ResetPassword;
