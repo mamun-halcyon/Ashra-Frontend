9715672388aed7d677a7549ec851cfa50da9fe69
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-20 15:59:27 +0600

    list product

--- src/components/list-card/index.tsx
+++ src/components/list-card/index.tsx
@@ -4,7 +4,8 @@ import './index.scss';
 import StarRating from '../rating';
 import Button from '../button';
 import Link from 'next/link';
-import { BsArrowRepeat } from 'react-icons/bs';
+import { BsArrowRepeat, BsHeart } from 'react-icons/bs';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 interface IProduct {
   title: string;
@@ -17,20 +18,20 @@ interface IProps {
   product: IProduct;
 }
 
-const ListCard: FC<IProps> = () => {
+const ListCard: FC<IProps> = ({ product }) => {
   return (
     <div className="list-card flex py-5">
-      <div className="image w-[30%] relative px-5 flex items-center">
+      <div className="image w-[30%] relative flex items-center justify-center">
         <Image
-          className="w-full"
-          src={'/assets/images/products/image1.png'}
+          className="w-[70%] object-cover"
+          src={product.image}
           width={400}
           height={400}
           alt="product"
         />
-        <div className=" absolute right-2 top-0">
+        <div className=" absolute right-4 top-0">
           <span className=" sudo inline-block discount font-gotham text-xs font-bold  px-2 py-1  rounded text-primary">
-            -12%
+            12%
           </span>
           <span className=" sudo inline-block new font-gotham text-xs font-bold  px-2 py-1  rounded text-primary">
             New
@@ -54,13 +55,29 @@ const ListCard: FC<IProps> = () => {
           Avail Bank EMI
         </Link>
 
-        <div className="flex">
-          <div className="flex items-center">
+        <div className="flex mt-7">
+          <div className="flex items-center mr-2">
             <div className="icon-area p-1">
-              <BsArrowRepeat className=" text-sm text-primary" />
+              <BsArrowRepeat className=" text-xs icon" />
             </div>
-            <p className="font-gotham font-normal text-xs text-black">
+            <p className="font-gotham font-normal text-xs icon-title">
               Compare
+            </p>
+          </div>
+          <div className="flex items-center mr-2">
+            <div className="icon-area p-1">
+              <BsHeart className=" text-xs icon" />
+            </div>
+            <p className="font-gotham font-normal text-xs icon-title">
+              Wishlist
+            </p>
+          </div>
+          <div className="flex items-center mr-2">
+            <div className="icon-area p-1">
+              <AiOutlinePlus className=" text-xs icon" />
+            </div>
+            <p className="font-gotham font-normal text-xs icon-title">
+              Add To Cart
             </p>
           </div>
         </div>
@@ -78,7 +95,7 @@ const ListCard: FC<IProps> = () => {
             Save ৳ 1000
           </span>
         </div>
-        <Button className="w-full font-gotham font-bold text-sm py-2 mt-4">
+        <Button className="w-full font-gotham font-bold text-sm py-2 mt-4 product-btn">
           Buy Now
         </Button>
       </div>
