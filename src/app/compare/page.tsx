a5d364bb3101c88d0df855eb4bfd0bb61973a6ed
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-21 16:22:30 +0600

    khusi

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import './page.scss';
 
 function Compare() {
-  return <div>Compare</div>;
+  return (
+    <section className="compare">
+      <div className="container">
+        <div className="header py-6">
+          <div className="flex">
+            <div className="w-[15%]"></div>
+            <div className="w-[21%]">
+              <h4 className=" font-gotham font-medium text-xs text-black">
+                GH-8203M - Gazi Smiss Gas..
+              </h4>
+            </div>
+            <div className="w-[21%]">
+              <h4 className=" font-gotham font-medium text-xs text-black">
+                GH-8203M - Gazi Smiss Gas..
+              </h4>
+            </div>
+            <div className="w-[21%]">
+              <h4 className=" font-gotham font-medium text-xs text-black">
+                GH-8203M - Gazi Smiss Gas..
+              </h4>
+            </div>
+            <div className="w-[21%]">
+              <h4 className=" font-gotham font-medium text-xs text-black">
+                GH-8203M - Gazi Smiss Gas..
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="w-[15%] pt-3 px-2">
+            <h3 className=" font-gotham font-medium text-xs text-black">
+              Products Comparison
+            </h3>
+            <p className="font-gotham font-normal text-xs text-black mt-2">
+              Find and select products tosee the differences and similarities
+              between them
+            </p>
+          </div>
+          <div className="w-[21%]">2</div>
+          <div className="w-[21%]">2</div>
+          <div className="w-[21%]">2</div>
+          <div className="w-[21%]">2</div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Compare;
