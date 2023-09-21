124dbba0b8f8d18efb253bf9bf64ec3e1c56d6e5
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-21 13:13:55 +0600

    wish list page completed

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,10 +1,160 @@
+import Button from '@/components/button';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { GoDotFill } from 'react-icons/go';
+import { RxCross2 } from 'react-icons/rx';
+import './page.scss';
 
 function WishlistPage() {
   return (
     <main>
-      <section>
-        <div className="container">wishlist</div>
+      <section className="wishlist-page">
+        <div className="container">
+          <h2 className=" font-gotham font-semibold text-lg wish-heading mb-1 pb-1">
+            Wishlist
+          </h2>
+          <div className="cart-elements">
+            <div className="grid grid-cols-8 gap-4 product-title">
+              <div className=" col-span-4 flex items-center justify-center">
+                <GoDotFill className="dot-icon" />
+                <h3 className=" font-gotham font-bold text-base text-black text-center">
+                  Product Name
+                </h3>
+              </div>
+              <div className="col-span-1 flex items-center ">
+                <GoDotFill className="dot-icon" />
+                <h3 className=" font-gotham font-bold text-base text-black text-center">
+                  Price
+                </h3>
+              </div>
+              <div className="col-span-2 flex items-center">
+                <GoDotFill className="dot-icon" />
+                <h3 className=" font-gotham font-bold text-base text-black text-center">
+                  Stock Status
+                </h3>
+              </div>
+              <div className="col-span-1 flex items-center"></div>
+            </div>
+            {/* single cart */}
+            <div className="grid grid-cols-8 gap-4 items-center product-item">
+              <div className="col-span-4">
+                <div className="flex items-center">
+                  <div className=" cursor-pointer">
+                    <span>
+                      <RxCross2 className="text-xs " />
+                    </span>
+                  </div>
+                  <div className="w-[80px] mx-9">
+                    <Image
+                      className=" w-full object-cover"
+                      src={'/assets/images/products/image3.png'}
+                      width={200}
+                      height={200}
+                      alt="product"
+                    />
+                  </div>
+                  <div>
+                    <h3 className=" font-gotham font-normal text-sm text-black">
+                      Gazi Smiss Gas Stove | B-239
+                    </h3>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-1">
+                <p className=" font-gotham font-medium text-primary text-xs">
+                  ৳ 3000.00
+                </p>
+              </div>
+              <div className="col-span-2">
+                <h3 className=" font-gotham font-medium text-sm">Instock</h3>
+              </div>
+              <div className="col-span-1">
+                <Button className="px-6 py-1 font-gotham font-medium text-sm">
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+            {/* single cart */}
+            <div className="grid grid-cols-8 gap-4 items-center product-item">
+              <div className="col-span-4">
+                <div className="flex items-center">
+                  <div className=" cursor-pointer">
+                    <span>
+                      <RxCross2 className="text-xs " />
+                    </span>
+                  </div>
+                  <div className="w-[80px] mx-9">
+                    <Image
+                      className=" w-full object-cover"
+                      src={'/assets/images/products/image4.png'}
+                      width={200}
+                      height={200}
+                      alt="product"
+                    />
+                  </div>
+                  <div>
+                    <h3 className=" font-gotham font-normal text-sm text-black">
+                      Gazi Smiss Gas Stove | B-239
+                    </h3>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-1">
+                <p className=" font-gotham font-medium text-primary text-xs">
+                  ৳ 3000.00
+                </p>
+              </div>
+              <div className="col-span-2">
+                <h3 className=" font-gotham font-medium text-sm">Stockout</h3>
+              </div>
+              <div className="col-span-1">
+                <Button className="px-6 py-1 font-gotham font-medium text-sm">
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+            {/* single cart */}
+            <div className="grid grid-cols-8 gap-4 items-center product-item">
+              <div className="col-span-4">
+                <div className="flex items-center">
+                  <div className=" cursor-pointer">
+                    <span>
+                      <RxCross2 className="text-xs " />
+                    </span>
+                  </div>
+                  <div className="w-[80px] mx-9">
+                    <Image
+                      className=" w-full object-cover"
+                      src={'/assets/images/products/image3.png'}
+                      width={200}
+                      height={200}
+                      alt="product"
+                    />
+                  </div>
+                  <div>
+                    <h3 className=" font-gotham font-normal text-sm text-black">
+                      Gazi Smiss Gas Stove | B-239
+                    </h3>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-1">
+                <p className=" font-gotham font-medium text-primary text-xs">
+                  ৳ 3000.00
+                </p>
+              </div>
+              <div className="col-span-2">
+                <h3 className=" font-gotham font-medium text-sm">Instock</h3>
+              </div>
+              <div className="col-span-1">
+                <Button className="px-6 py-1 font-gotham font-medium text-sm">
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
