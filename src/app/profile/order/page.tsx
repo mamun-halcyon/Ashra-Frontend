4ec58e351866652f2ab766474cf8845aaa9e2e96
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-27 20:42:57 +0600

    order

--- src/app/profile/order/page.tsx
+++ src/app/profile/order/page.tsx
@@ -9,15 +9,59 @@ const OrderHistory = () => {
           <ProfileSidebar />
           <div className=" col-span-9">
             <div className="shadow">
-              <table className="table-fixed">
-                <tbody>
+              <table className="w-full text-sm text-left ">
+                <thead>
                   <tr>
-                    <th>Code</th>
-                    <th>Date</th>
-                    <th>Amount</th>
-                    <th>Delivery Status</th>
-                    <th>Payment Status</th>
-                    <th>Payment Status</th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-normal"
+                    >
+                      Code
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-normal"
+                    >
+                      Date
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-normal"
+                    >
+                      Amount
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-normal"
+                    >
+                      Delivery Status
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-normal"
+                    >
+                      Payment Status
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-normal"
+                    >
+                      Options
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Code
+                    </th>
+                    <td className="px-6 py-4">Date</td>
+                    <td className="px-6 py-4">Amount</td>
+                    <td className="px-6 py-4">Delivery Status</td>
+                    <td className="px-6 py-4">Payment Status</td>
                   </tr>
                 </tbody>
               </table>
