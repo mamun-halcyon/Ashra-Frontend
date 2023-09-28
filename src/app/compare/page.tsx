8f1736674bbee8ee50d43d7d8268e9bc574a8160
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-28 20:16:45 +0600

    improve compare

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './page.scss';
 import Button from '@/components/button';
+import Image from 'next/image';
+import { RxCrossCircled } from 'react-icons/rx';
+import StarRating from '@/components/rating';
 
 function Compare() {
   return (
@@ -60,7 +63,7 @@ function Compare() {
               GH-8203M - Gazi Smiss Gas..
             </h4>
           </div>
-          <div className="flex p-3 pb-0">
+          <div className="flex p-3 pb-0 product-card">
             <div className="pt-3 px-2">
               <h3 className=" font-gotham font-medium text-xs text-black">
                 Products Comparison
@@ -71,6 +74,169 @@ function Compare() {
               </p>
             </div>
           </div>
+          <div className="p-4 product-card">
+            <p className=" font-gotham font-light text-xs text-black">
+              Kitchen Hood
+            </p>
+            <p className=" font-gotham font-light text-xs danger-text">
+              <span className="mt-1">
+                <RxCrossCircled className="inline mr-1" />
+              </span>
+              Remove
+            </p>
+            <h3 className=" font-gotham font-medium text-xs my-3">
+              GH-8203M - Gazi Smiss Gas Stove
+            </h3>
+            <Image
+              className=" w-3/4 mx-auto my-3"
+              src={'/assets/images/products/image1.png'}
+              width={150}
+              height={150}
+              alt="product"
+            />
+            <p className=" font-gotham font-normal text-xs line-through">
+              ৳ 7000
+            </p>
+            <div className="flex justify-between mt-2">
+              <h3 className=" font-gotham font-medium text-sm">৳ 6000</h3>
+              <Button className="px-2 font-gotham font-light text-sm">
+                Save ৳ 1000
+              </Button>
+            </div>
+          </div>
+          <div className="p-4 product-card">
+            <p className=" font-gotham font-light text-xs text-black">
+              Kitchen Hood
+            </p>
+            <p className=" font-gotham font-light text-xs danger-text">
+              <span className="mt-1">
+                <RxCrossCircled className="inline mr-1" />
+              </span>
+              Remove
+            </p>
+            <h3 className=" font-gotham font-medium text-xs my-3">
+              GH-8203M - Gazi Smiss Gas Stove
+            </h3>
+            <Image
+              className=" w-3/4 mx-auto my-3"
+              src={'/assets/images/products/image1.png'}
+              width={150}
+              height={150}
+              alt="product"
+            />
+            <p className=" font-gotham font-normal text-xs line-through">
+              ৳ 7000
+            </p>
+            <div className="flex justify-between mt-2">
+              <h3 className=" font-gotham font-medium text-sm">৳ 6000</h3>
+              <Button className="px-2 font-gotham font-light text-sm">
+                Save ৳ 1000
+              </Button>
+            </div>
+          </div>
+          <div className="p-4 product-card">
+            <p className=" font-gotham font-light text-xs text-black">
+              Kitchen Hood
+            </p>
+            <p className=" font-gotham font-light text-xs danger-text">
+              <span className="mt-1">
+                <RxCrossCircled className="inline mr-1" />
+              </span>
+              Remove
+            </p>
+            <h3 className=" font-gotham font-medium text-xs my-3">
+              GH-8203M - Gazi Smiss Gas Stove
+            </h3>
+            <Image
+              className=" w-3/4 mx-auto my-3"
+              src={'/assets/images/products/image1.png'}
+              width={150}
+              height={150}
+              alt="product"
+            />
+            <p className=" font-gotham font-normal text-xs line-through">
+              ৳ 7000
+            </p>
+            <div className="flex justify-between mt-2">
+              <h3 className=" font-gotham font-medium text-sm">৳ 6000</h3>
+              <Button className="px-2 font-gotham font-light text-sm">
+                Save ৳ 1000
+              </Button>
+            </div>
+          </div>
+          <div className="p-4 product-card">
+            <p className=" font-gotham font-light text-xs text-black">
+              Kitchen Hood
+            </p>
+            <p className=" font-gotham font-light text-xs danger-text">
+              <span className="mt-1">
+                <RxCrossCircled className="inline mr-1" />
+              </span>
+              Remove
+            </p>
+            <h3 className=" font-gotham font-medium text-xs my-3">
+              GH-8203M - Gazi Smiss Gas Stove
+            </h3>
+            <Image
+              className=" w-3/4 mx-auto my-3"
+              src={'/assets/images/products/image1.png'}
+              width={150}
+              height={150}
+              alt="product"
+            />
+            <p className=" font-gotham font-normal text-xs line-through">
+              ৳ 7000
+            </p>
+            <div className="flex justify-between mt-2">
+              <h3 className=" font-gotham font-medium text-sm">৳ 6000</h3>
+              <Button className="px-2 font-gotham font-light text-sm">
+                Save ৳ 1000
+              </Button>
+            </div>
+          </div>
+          <div className="p-4 product-row">
+            <h3 className=" font-gotham font-medium text-black text-xs">
+              Brand
+            </h3>
+          </div>
+          <div className="p-4 product-row">
+            <h3 className=" font-gotham font-medium text-black text-xs">
+              Gazi
+            </h3>
+          </div>
+          <div className="p-4 product-row">
+            <h3 className=" font-gotham font-medium text-black text-xs">
+              Gazi
+            </h3>
+          </div>
+          <div className="p-4 product-row">
+            <h3 className=" font-gotham font-medium text-black text-xs">
+              Gazi
+            </h3>
+          </div>
+          <div className="p-4 product-row">
+            <h3 className=" font-gotham font-medium text-black text-xs">
+              Gazi
+            </h3>
+          </div>
+          <div className="p-4 product-card">
+            <h3 className=" font-gotham font-medium text-black text-xs">
+              Rating
+            </h3>
+          </div>
+          <div className="p-4 product-card">
+            <StarRating rating={4} />
+          </div>
+          <div className="p-4 product-card">
+            <StarRating rating={4} />
+          </div>
+          <div className="p-4 product-card">
+            <StarRating rating={4} />
+          </div>
+          <div className="p-4 product-card">
+            <StarRating rating={4} />
+          </div>
+          <div className="p-4">Brand</div>
           <h1>1023</h1>
           <h1>1023</h1>
           <h1>1023</h1>
