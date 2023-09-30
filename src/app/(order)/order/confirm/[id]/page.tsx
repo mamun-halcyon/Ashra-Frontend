f20b170e555a78b518bbc151fd0907e0d53032c7
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-30 11:00:24 +0600

    confirm order

---
+++ src/app/(order)/order/confirm/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Invoice from '@/components/invoice';
+import React from 'react';
+
+const ConfirmOrder = () => {
+  return (
+    <div className="container">
+      <div>ConfirmOrder</div>
+      <Invoice />
+    </div>
+  );
+};
+
+export default ConfirmOrder;
