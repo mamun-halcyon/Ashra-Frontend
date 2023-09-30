359ffb2078dcc0be8cc5c449f742935a01359fae
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-09-30 12:48:56 +0600

    final 04

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
               width={1300}
               height={500}
             />
-            <h2 className=" py-12 uppercase text-center font-gotham text-xl font-bold">
+            <h2 className=" py-12 uppercase text-center font-gotham text-xl font-medium">
               PRODUCT REVIEWS & UNBOXING VIDEOS
             </h2>
             <div className="grid grid-cols-3 gap-4">
