ff7ddfbe844630e04d37eae591dfa109d8d64e50
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-11-15 16:13:37 +0600

    change more redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,6 +5,7 @@ export const store = configureStore({
   reducer: {
     product: productReducer,
   },
+  devTools: true,
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
