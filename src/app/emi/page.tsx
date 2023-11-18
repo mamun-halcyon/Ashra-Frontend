22b9a5087677d6038af15696f50106f68afb9da9
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-11-18 14:21:54 +0600

    emi pages

---
+++ src/app/emi/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './page.scss';
+
+function Emi() {
+  return (
+    <section className="emi">
+      <div className="container">Emi</div>
+    </section>
+  );
+}
+
+export default Emi;
