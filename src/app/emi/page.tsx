e4f7d1dd356a005f7317d37a321fe6520e60e0ce
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-11-18 18:34:26 +0600

    complete emi page

--- src/app/emi/page.tsx
+++ src/app/emi/page.tsx
@@ -4,7 +4,275 @@ import './page.scss';
 function Emi() {
   return (
     <section className="emi">
-      <div className="container">Emi</div>
+      <div className="container md:overflow-x-auto overflow-x-scroll">
+        <table className="w-full text-sm text-left">
+          <thead>
+            <tr className="table-heading">
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                Bank Name
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                3 Month
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                6 Month
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                9 Month
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                12 Month
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                18 Month
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                24 Month
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                30 Month
+              </th>
+              <th scope="col" className="px-6 py-3 font-gotham font-medium">
+                36 Month
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+            <tr className="table-border">
+              <td scope="row " className="px-6 py-3 font-gotham font-light">
+                Jamuna Bank Limited
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                Yes
+              </td>
+              <td scope="row" className="px-6 py-3 font-gotham font-light">
+                NO
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
