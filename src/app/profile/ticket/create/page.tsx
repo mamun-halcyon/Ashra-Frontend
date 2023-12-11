e2cec4ed0674aa0702aaa585db1a9368b46c0b73
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-12-11 20:54:23 +0600

    order details page

--- src/app/profile/ticket/create/page.tsx
+++ src/app/profile/ticket/create/page.tsx
@@ -1,9 +1,10 @@
-import Button from "@/components/button";
-import FormGroup from "@/components/fromgroup";
-import ProfileSidebar from "@/components/profile-sidebar";
-import { useAppSelector } from "@/redux/hooks";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+'use client';
+import Button from '@/components/button';
+import FormGroup from '@/components/fromgroup';
+import ProfileSidebar from '@/components/profile-sidebar';
+import { useAppSelector } from '@/redux/hooks';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 const CreateTicket = () => {
   const route = useRouter();
@@ -14,7 +15,7 @@ const CreateTicket = () => {
     if (login?.accessToken) {
       setIsLoggedIn(true);
     } else {
-      route.push("/login");
+      route.push('/login');
     }
   }, [login]);
 
--- src/components/single-order/index.tsx
+++ src/components/single-order/index.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import Button from '../button';
 import { LiaDownloadSolid, LiaEye } from 'react-icons/lia';
+import { RxCross2 } from 'react-icons/rx';
+import './index.scss';
 
 const SingleOrder = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,15 +24,81 @@ const SingleOrder = () => {
           </div>
           <LiaDownloadSolid className="cursor-pointer" />
           {isOpen && (
-            <div className="refund-request shadow-lg">
-              <div className="text-right">
+            <div className="refund-request shadow-lg mb-4">
+              <div className="flex justify-end">
                 <span
-                  className=" cursor-pointer"
+                  className=" cursor-pointer ml-auto"
                   onClick={() => setIsOpen(false)}
                 >
-                  X
+                  <RxCross2 />
                 </span>
               </div>
+
+              <div className="flex justify-between mb-4">
+                <div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Order Code:
+                    </p>
+                    <p className=" font-gotham text-sm">GHA-21</p>
+                  </div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Customer Name:
+                    </p>
+                    <p className=" font-gotham text-sm">John</p>
+                  </div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">Email:</p>
+                    <p className=" font-gotham text-sm">example@gmail.com</p>
+                  </div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Shipping Address:{' '}
+                    </p>
+                    <p className=" font-gotham text-sm">
+                      340, Road5, Ave3, Mirpur Dohs
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Order Date :{' '}
+                    </p>
+                    <p className=" font-gotham text-sm ml-2">
+                      12-12-2023 02:43AM
+                    </p>
+                  </div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Order Status:{' '}
+                    </p>
+                    <p className=" font-gotham text-sm ml-2">Pending</p>
+                  </div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Total Order Amount:{' '}
+                    </p>
+                    <p className=" font-gotham text-sm ml-2">2302</p>
+                  </div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Shipping method:{' '}
+                    </p>
+                    <p className=" font-gotham text-sm ml-2">Flat Shipping</p>
+                  </div>
+                  <div className="flex py-1">
+                    <p className=" font-gotham text-sm font-semibold">
+                      Payment method:{' '}
+                    </p>
+                    <p className=" font-gotham text-sm ml-2">
+                      Cash on Delivery
+                    </p>
+                  </div>
+                </div>
+              </div>
+
               <table className="w-full text-sm text-left ">
                 <thead>
                   <tr className="table-heading">
@@ -50,6 +118,18 @@ const SingleOrder = () => {
                       scope="col"
                       className="px-6 py-3 font-gotham font-medium"
                     >
+                      Variant
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-medium"
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 font-gotham font-medium"
+                    >
                       Warrenty/Refund
                     </th>
                   </tr>
@@ -58,12 +138,47 @@ const SingleOrder = () => {
                   <tr className=" font-normal font-gotham text-sm table-border p-2">
                     <td className="px-6 py-4">Product Name</td>
                     <td className="px-6 py-4">2</td>
+                    <td className="px-6 py-4">LPG</td>
+                    <td className="px-6 py-4">৳2000</td>
+                    <td className="px-6 py-4">
+                      <Button className="px-3 py-1">Apply Now</Button>
+                    </td>
+                  </tr>
+                  <tr className=" font-normal font-gotham text-sm table-border p-2">
+                    <td className="px-6 py-4">Product Name</td>
+                    <td className="px-6 py-4">2</td>
+                    <td className="px-6 py-4">LPG</td>
+                    <td className="px-6 py-4">৳2000</td>
                     <td className="px-6 py-4">
                       <Button className="px-3 py-1">Apply Now</Button>
                     </td>
                   </tr>
                 </tbody>
               </table>
+              <div className="mt-12">
+                <div className="flex justify-end py-1">
+                  <div>
+                    <h3 className=" font-gotham text-sm font-semibold mb-3">
+                      Subtotal:
+                    </h3>
+                    <h3 className=" font-gotham text-sm font-semibold mb-3">
+                      Shipping:
+                    </h3>
+                    <h3 className=" font-gotham text-sm font-semibold mb-3">
+                      Coupon:
+                    </h3>
+                    <h3 className=" font-gotham text-sm font-semibold mb-3">
+                      Total:
+                    </h3>
+                  </div>
+                  <div className="ml-6">
+                    <p className=" font-gotham text-sm mb-3">৳1200</p>
+                    <p className=" font-gotham text-sm mb-3">৳120</p>
+                    <p className=" font-gotham text-sm mb-3">৳200</p>
+                    <p className=" font-gotham text-sm mb-3">৳1000</p>
+                  </div>
+                </div>
+              </div>
             </div>
           )}
         </div>
