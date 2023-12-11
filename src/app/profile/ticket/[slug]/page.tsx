ea767dd9051596dc0a177911db176d3731e0c2bb
Halcyon-Digital <zunayed.ali417@gmail.com>
2023-12-11 21:19:31 +0600

    ticket replay

--- src/app/profile/ticket/[slug]/page.tsx
+++ src/app/profile/ticket/[slug]/page.tsx
@@ -5,6 +5,7 @@ import ProfileSidebar from '@/components/profile-sidebar';
 import { useAppSelector } from '@/redux/hooks';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
+import './page.scss';
 
 const ViewTicket = () => {
   const route = useRouter();
@@ -27,16 +28,25 @@ const ViewTicket = () => {
             <div className="grid grid-cols-12 gap-6">
               <ProfileSidebar />
               <div className=" col-span-9">
-                <form className="w-[450px] mx-auto">
-                  <FormGroup
-                    className="mt-2 "
-                    type="email"
-                    title="Subject*"
-                    placeholder="subject"
-                    required
-                  />
-
+                <div className="mb-12 h-[350px] overflow-y-scroll scrollbar">
+                  <div className="py-2">
+                    <p className=" font-gotham text-sm bg-hoverColor text-white inline-block px-2 py-1 rounded-md">
+                      Hello
+                    </p>
+                  </div>
+                  <div className="text-right py-2">
+                    <p className=" font-gotham text-sm bg-primary text-white inline-block px-2 py-1 rounded-md">
+                      Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                </div>
+                <form className="mx-auto">
+                  <label
+                    className=" font-gotham text-sm"
+                    htmlFor="message"
+                  ></label>
                   <textarea
+                    id="message"
                     className="border border-secondary mt-3 w-full p-2 font-gotham text-xs outline-none min-h-[100px]"
                     placeholder="Your replay"
                   ></textarea>
@@ -48,11 +58,12 @@ const ViewTicket = () => {
                     placeholder="Photo"
                     required
                   />
-                  <Button className="mt-2 px-3 py-1 font-gotham text-xs">
-                    Replay
-                  </Button>
+                  <div className="text-right">
+                    <Button className="mt-2 px-3 py-1 font-gotham text-xs">
+                      Replay
+                    </Button>
+                  </div>
                 </form>
-                <div>Message Here</div>
               </div>
             </div>
           </div>
