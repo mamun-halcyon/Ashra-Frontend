42665230a73644f2b7272f1ee8b1361a4cd0a32a
Halcyon-Digital-24 <halcyonbd18@gmail.com>
2024-04-27 20:47:04 +0600

    maintenance page added

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./page.scss";
+import Image from "next/image";
+
+const Maintenance = () => {
+  return (
+    <div className=" md:py-40 py-12 text-center">
+      <Image
+        className="maintenance-loader"
+        src={"/assets/images/icon/gear-rotation.svg"}
+        width={800}
+        height={800}
+        alt="loader"
+      />
+
+      <h2 className=" font-gotham font-medium text-lg primary-text">
+        Site is under maintenance
+      </h2>
+      <p className=" font-gotham text-sm mt-2">
+        We're working hard to improve the user experience. Stay turned
+      </p>
+    </div>
+  );
+};
+
+export default Maintenance;
