f52a0fae66e893c3fcb9bd2197c4102024eb3355
G. M. FARHAD HOSSAIN <mdhabiburrb@gmail.com>
2024-05-23 09:50:19 +0600

    invoice paid note

--- src/components/invoice/index.tsx
+++ src/components/invoice/index.tsx
@@ -49,160 +49,179 @@ const Invoice = ({
   return (
     <div className="invoice white-bg hidden">
       <div className="invoice-body">
-      <div className="invoice-header font-gotham text-xs" >
-        <div className="title">
-          {order.order_prefix === "GHA" ? (
-            <img src="/assets/invoice/homeappliance.png" alt="invoice" />
-          ) : (
-            <img src="/assets/invoice/pump.png" alt="invoice" />
-          )}
-        </div>
-        <h4 className="customer-details font-gotham font-medium">
-          Customer Details
-        </h4>
-        <div className="details">
-          <div className="left">
-            <p>
-              <span className="invoice-title">Invoice:</span>{" "}
-              {order.order_prefix}-{order.id}
-            </p>
-            <p>
-              {" "}
-              <span className="invoice-title">Name: </span> {order.name}
-            </p>
-            <p>
-              <span className="invoice-title">Email: </span> {order.email}
-            </p>
-            <p>
-              <span className="invoice-title">Phone: </span>{" "}
-              {`+88${order.mobile}`}{" "}
-            </p>
-            <p>
-              <span className="invoice-title"> Address: </span> {order.address}{" "}
-              {order.city ? `, ${order.city}` : ""}{" "}
-            </p>
+        <div className="invoice-header font-gotham text-xs" >
+          <div className="title">
+            {order.order_prefix === "GHA" ? (
+              <img src="/assets/invoice/homeappliance.png" alt="invoice" />
+            ) : (
+              <img src="/assets/invoice/pump.png" alt="invoice" />
+            )}
           </div>
-          <div className="order-details right">
-            <p>
-              <span className="invoice-title">Order Date: </span>{" "}
-              {formatDate(order.created_at)}
-            </p>
-            <p>
-              <span className="invoice-title"> Order Status:</span>{" "}
-              {order?.order_status}
-            </p>
-            <p>
-              <span className="invoice-title"> Total Order Amount : </span>{" "}
-              {totalPrice + order.delivery_fee - order.custom_discount}
-            </p>
-            <p>
-              <span className="invoice-title"> Shipping Method: </span>{" "}
-              {order?.delivery_method === "homeDelivery"
-                ? "Free Delivery"
-                : "Express Delivery"}
-            </p>
-            <p>
-              <span className="invoice-title"> Payment Method: </span>{" "}
-              {order?.payment_method === "cod"
-                ? "Cash on Delivery"
-                : "Online Payment"}
-            </p>
-            <p>
-              <span className="invoice-title"> Payment Status: </span>{" "}
-              {order?.payment_status}
-            </p>
+          <h4 className="customer-details font-gotham font-medium">
+            Customer Details
+          </h4>
+          <div className="details">
+            <div className="left">
+              <p>
+                <span className="invoice-title">Invoice:</span>{" "}
+                {order.order_prefix}-{order.id}
+              </p>
+              <p>
+                {" "}
+                <span className="invoice-title">Name: </span> {order.name}
+              </p>
+              <p>
+                <span className="invoice-title">Email: </span> {order.email}
+              </p>
+              <p>
+                <span className="invoice-title">Phone: </span>{" "}
+                {`+88${order.mobile}`}{" "}
+              </p>
+              <p>
+                <span className="invoice-title"> Address: </span> {order.address}{" "}
+                {order.city ? `, ${order.city}` : ""}{" "}
+              </p>
+            </div>
+            <div className="order-details right">
+              <p>
+                <span className="invoice-title">Order Date: </span>{" "}
+                {formatDate(order.created_at)}
+              </p>
+              <p>
+                <span className="invoice-title"> Order Status:</span>{" "}
+                {order?.order_status}
+              </p>
+              <p>
+                <span className="invoice-title"> Total Order Amount : </span>{" "}
+                {totalPrice + order.delivery_fee - order.custom_discount}
+              </p>
+              <p>
+                <span className="invoice-title"> Shipping Method: </span>{" "}
+                {order?.delivery_method === "homeDelivery"
+                  ? "Free Delivery"
+                  : "Express Delivery"}
+              </p>
+              <p>
+                <span className="invoice-title"> Payment Method: </span>{" "}
+                {order?.payment_method === "cod"
+                  ? "Cash on Delivery"
+                  : "Online Payment"}
+              </p>
+              <p>
+                <span className="invoice-title"> Payment Status: </span>{" "}
+                {order?.payment_status}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <table className="invoice-details-table" style={{background:'green'}}>
-        <tr className="table-heading">
-          <th>SL.</th>
-          <th>Description</th>
-          <th>Attribute</th>
-          <th>Qty</th>
-          <th>Unit price (BDT)</th>
-          <th>Total</th>
-        </tr>
-        {orderItems?.length > 0 &&
-          orderItems?.map((product, index) => (
-            <tr key={index} className="order-item">
-              <td>{index + 1}</td>
-              <td>{product.product_name}</td>
-              <td>
-                {/* Attribute */}
-                {product.product_attribute
-                  ? JSON.parse(product.product_attribute).map(
+        <table className="invoice-details-table" style={{ background: 'green' }}>
+          <tr className="table-heading">
+            <th>SL.</th>
+            <th>Description</th>
+            <th>Attribute</th>
+            <th>Qty</th>
+            <th>Unit price (BDT)</th>
+            <th>Total</th>
+          </tr>
+          {orderItems?.length > 0 &&
+            orderItems?.map((product, index) => (
+              <tr key={index} className="order-item">
+                <td>{index + 1}</td>
+                <td>{product.product_name}</td>
+                <td>
+                  {/* Attribute */}
+                  {product.product_attribute
+                    ? JSON.parse(product.product_attribute).map(
                       (v: any, i: number) => (
                         <span className="variant" key={i}>
                           {`${i ? "," : ""}${v.attribute_name}`}
                         </span>
                       )
                     )
-                  : "-"}
-              </td>
-              <td> {product.quantity}</td>
-              <td>{FormatPrice(product.regular_price)}</td>
-              <td> {FormatPrice(product.regular_price * product.quantity)}</td>
-            </tr>
-          ))}
-        <tr>
-          <td className="span-item" colSpan={4}></td>
-          <td className="heading-title">Sub Total</td>
-          <td>{` ${FormatPrice(amountBeforeCoupon)}`}</td>
-        </tr>
-        <tr>
-          <td className="span-item" colSpan={4}></td>
-          <td className="heading-title">Delivery</td>
-          <td>{FormatPrice(order.delivery_fee)}</td>
-        </tr>
-        <tr>
-          <td className="span-item" colSpan={4}></td>
-          <td className="heading-title">Discount</td>
-          <td>
-            {FormatPrice(
-              amountBeforeCoupon - totalPrice + order.custom_discount
-            )}
-          </td>
-        </tr>
-        <tr>
-          <td className="span-item" colSpan={4}></td>
-          <td className="heading-title">Advance</td>
-          <td>{FormatPrice(order.advance_payment ?? 0)}</td>
-        </tr>
-        <tr>
-          <td className="span-item" colSpan={4}></td>
-          <td className="heading-title">Due Amount</td>
-          <td>
-            {FormatPrice(
-              totalPrice +
+                    : "-"}
+                </td>
+                <td> {product.quantity}</td>
+                <td>{FormatPrice(product.regular_price)}</td>
+                <td> {FormatPrice(product.regular_price * product.quantity)}</td>
+              </tr>
+            ))}
+          <tr>
+            <td className="span-item" colSpan={4}></td>
+            <td className="heading-title">Sub Total</td>
+            <td>{` ${FormatPrice(amountBeforeCoupon)}`}</td>
+          </tr>
+          <tr>
+            <td className="span-item" colSpan={4}></td>
+            <td className="heading-title">Delivery</td>
+            <td>{FormatPrice(order.delivery_fee)}</td>
+          </tr>
+          <tr>
+            <td className="span-item" colSpan={4}></td>
+            <td className="heading-title">Discount</td>
+            <td>
+              {FormatPrice(
+                amountBeforeCoupon - totalPrice + order.custom_discount
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="span-item" colSpan={4}></td>
+            <td className="heading-title">Advance</td>
+            <td>{FormatPrice(order.advance_payment ?? 0)}</td>
+          </tr>
+          <tr>
+            <td className="span-item" colSpan={4}></td>
+            <td className="heading-title">Due Amount</td>
+            <td>
+              {FormatPrice(
+                totalPrice +
                 order.delivery_fee -
                 order.custom_discount -
                 advancePayment
-            )}
-          </td>
-        </tr>
-      </table>
-      <div className="notes" style={{background:'red'}}>
-        <h3 className=" font-gotham font-medium text-sm">Notes:</h3>
-        <p className=" font-gotham text-xs font-light">
-          1. All our products come with a{" "}
-          {order.order_prefix === "GHA" ? "one-year" : "two-years"} service
-          warranty. To claim the warranty, please present this invoice.
-        </p>
-        <p className=" font-gotham text-xs font-light">
-          2. Please ensure to check for any physical damage to the product upon
-          receiving it. After receiving the product, no claims for physical
-          damage will be accepted.
-        </p>
-        <p className=" font-gotham text-xs font-light">
-          3. For official installation, please inform us upon receiving the
-          product if the customer wishes for us to install it. We will require
-          24 hours to complete the installation.
-        </p>
+              )}
+            </td>
+          </tr>
+
+          <div className="payment-status">
+            {
+              order?.payment_status == 'paid' || order?.payment_status == "Paid" ?
+
+                <div className="img-sec">
+                  <img src="/assets/invoice/paid.png" alt="invoice" />
+
+                </div> : ''
+            }
+            {
+              order?.payment_status == 'Paid' || order?.payment_status == "paid" ?
+                <span>
+                  {order?.note}
+                </span> : ''
+            }
+
+
+          </div>
+        </table>
+        <div className="notes" style={{ background: 'red' }}>
+          <h3 className=" font-gotham font-medium text-sm">Notes:</h3>
+          <p className=" font-gotham text-xs font-light">
+            1. All our products come with a{" "}
+            {order.order_prefix === "GHA" ? "one-year" : "two-years"} service
+            warranty. To claim the warranty, please present this invoice.
+          </p>
+          <p className=" font-gotham text-xs font-light">
+            2. Please ensure to check for any physical damage to the product upon
+            receiving it. After receiving the product, no claims for physical
+            damage will be accepted.
+          </p>
+          <p className=" font-gotham text-xs font-light">
+            3. For official installation, please inform us upon receiving the
+            product if the customer wishes for us to install it. We will require
+            24 hours to complete the installation.
+          </p>
+        </div>
       </div>
-      </div>
-      <div className="invoice-footer" style={{background:'green'}}>
+      <div className="invoice-footer" style={{ background: 'green' }}>
         <div className="title">
           {order.order_prefix === "GHA" ? (
             <img src="/assets/invoice/home-footer.png" alt="invoice" />
